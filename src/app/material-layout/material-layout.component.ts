9b9ab0bf454edf80389ca22f5556321bfd89a384
venkatesh pallanti <venkateshpallanti@bitapps.com>
2022-01-17 13:33:26 +0530

    packages are updated

---
+++ src/app/material-layout/material-layout.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { MatSidenav } from '@angular/material/sidenav';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'material-layout',
+  templateUrl: './material-layout.component.html',
+  styleUrls: ['./material-layout.component.scss']
+})
+export class MaterialLayoutComponent implements OnInit {
+  menuList: Observable<[]>;
+
+
+  public loading: boolean;
+  public isAuthenticated: boolean;
+  public title: string;
+
+  public isBypass: boolean;
+  public mobile: boolean;
+  public isMenuInitOpen: boolean;
+
+  constructor(private httpService: HttpClient,private breakpointObserver: BreakpointObserver,
+              private router: Router,
+              private _snackBar: MatSnackBar) { }
+
+    private sidenav: MatSidenav;
+
+    public isMenuOpen = true;
+    public contentMargin = 240;
+
+    get isHandset(): boolean {
+            return this.breakpointObserver.isMatched(Breakpoints.Handset);     
+    }
+
+  // *********************************************************************************************
+  // * LIFE CYCLE EVENT FUNCTIONS
+  // *********************************************************************************************
+
+    ngOnInit() {
+      this.isMenuOpen = true;  // Open side menu by default
+      this.title = 'Material Layout Demo';
+      this.menuList = this.httpService.get<any>("/assets/menu.json")
+    }
+
+    ngDoCheck() {
+        if (this.isHandset) {
+          this.isMenuOpen = false;
+        } else {
+          this.isMenuOpen = true;
+        }
+    }
+
+  // *********************************************************************************************
+  // * COMPONENT FUNCTIONS
+  // *********************************************************************************************
+
+  public openSnackBar(msg: string): void {
+    this._snackBar.open(msg, 'X', {
+      duration: 2000,
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+      panelClass: 'notif-error'
+    });
+  }
+
+  public onSelectOption(option: any): void {
+    const msg = `Chose option ${option}`;
+    this.openSnackBar(msg);
+
+    /* To route to another page from here */
+    // this.router.navigate(['/home']);
+  }
+}
